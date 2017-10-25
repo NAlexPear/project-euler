448ae393f458d4a0e07b43c40b6dcf6fa54db0a6
Alex Pearson <alex@alexpear.com>
2017-10-24 23:15:11 -0500

    Finish even fibonacci numbers

---
+++ haskell/02-even-fibonacci.hs
@@ -0,0 +1,16 @@
+fibonacci :: Int -> Int
+fibonacci x
+  | x <= 2 = x
+  | otherwise = fibonacci (x - 1) + fibonacci (x - 2)
+
+generateFibonacciSequence :: Int -> [Int]
+generateFibonacciSequence maxValue =
+  takeWhile (\x -> x < maxValue ) (map fibonacci [1..])
+
+sumEvens :: [Int] -> Int
+sumEvens numbers =
+  sum [x | x <- numbers, mod x 2 == 0]
+
+sumToFibLimit :: Int -> Int
+sumToFibLimit maxValue =
+  sumEvens $ generateFibonacciSequence maxValue
