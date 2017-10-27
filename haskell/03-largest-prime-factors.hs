5dc8e86ae461a8a05e0f035c79a1b8f4e8de6c48
Alex Pearson <alex@alexpear.com>
2017-10-27 15:50:49 -0500

    Finish optimized largest-prime-factors exercise

--- haskell/03-largest-prime-factors.hs
+++ haskell/03-largest-prime-factors.hs
@@ -1,3 +1,5 @@
+import Data.Maybe
+
 isPrime :: Int -> Bool
 isPrime num =
     let
@@ -20,8 +22,29 @@ getFactors num =
 
 getPrimeFactors :: Int -> [Int]
 getPrimeFactors num =
-  filter isPrime $ getFactors num
+  case factors of
+    [] -> [num]
+    _  -> factors ++ getPrimeFactors (div num $ head factors)
+  where factors = take 1 $ getFactors num 
+  
+
+getSmallestPrimeFactor :: Int -> [Int] 
+getSmallestPrimeFactor num =
+  take 1 $ getPrimeFactors num
 
 
-getLargestPrimeFactor =
-  maximum . getPrimeFactors
+getLargestPrimeFactor :: Int -> Int
+getLargestPrimeFactor num =
+  let
+    smallestPrime =
+      head $ getSmallestPrimeFactor num
+
+    limit =
+      div num smallestPrime
+
+    set =
+      takeWhile (limit >) $ getPrimeFactors num
+  in
+    case set of
+      [] -> limit
+      _ -> last set
